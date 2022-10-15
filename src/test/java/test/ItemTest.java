62cfa64d7bb5838861e2fa8f76a1b2231fa85229
Never Adrian Sossa <nadris.xhhj@gmail.com>
2022-10-15 16:52:37 -0400

    add crud item test

--- src/test/java/test/ItemTest.java
+++ src/test/java/test/ItemTest.java
@@ -1,4 +1,42 @@
 package test;
-
+import factoryRequest.FactoryRequest;
+import factoryRequest.RequestInfo;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import util.ApiConfiguration;
+import static org.hamcrest.Matchers.equalTo;
 public class ItemTest {
+    Response response;
+    JSONObject body= new JSONObject();
+    RequestInfo requestInfo = new RequestInfo();
+    @Test
+    public void VerifyCRUDItem(){
+
+        body.put("Content","New Item");
+        requestInfo.setUrl(ApiConfiguration.CREATE_ITEM);
+        requestInfo.setBody(body.toString());
+        response= FactoryRequest.make("post").send(requestInfo);
+        response.then().body("Content",equalTo(body.get("Content"))).statusCode(200);
+
+        int idItem=response.then().extract().path("Id");
+
+        requestInfo.setUrl(String.format(ApiConfiguration.READ_ITEM,idItem));
+        requestInfo.setBody(body.toString());
+        response= FactoryRequest.make("get").send(requestInfo);
+        response.then().body("Content",equalTo(body.get("Content"))).statusCode(200);
+
+        body.put("Content","New Item Updated");
+        requestInfo.setUrl(String.format(ApiConfiguration.UPDATE_ITEM,idItem));
+        requestInfo.setBody(body.toString());
+        response= FactoryRequest.make("put").send(requestInfo);
+        response.then().body("Content",equalTo(body.get("Content"))).statusCode(200);
+
+        requestInfo.setUrl(String.format(ApiConfiguration.DELETE_ITEM,idItem));
+        requestInfo.setBody(body.toString());
+        response= FactoryRequest.make("delete").send(requestInfo);
+        response.then().body("Content",equalTo(body.get("Content"))).statusCode(200);
+
+    }
+
 }
